51414505909b8176aedf702487c5612282baa619
Yupeng Zhang <yupeng.zhang@tum.de>
2024-02-25 11:51:10 +0100

    add python script for camera std

---
+++ src/ball_controller/scripts/ball_position_recorder.py
@@ -0,0 +1,73 @@
+import rospy
+import rosbag
+import pandas as pd
+from ball_controller.msg import PosVel2D
+from geometry_msgs.msg import Vector3Stamped
+
+class SkinForceRecorder:
+
+    def  __init__(self, bag_path, csv_path):
+
+        rospy.Subscriber('/ball_position', Vector3Stamped, self.callback)
+        self.bag = rosbag.Bag(bag_path, 'w')
+
+
+    def callback(self, PosVel2D):
+
+        self.bag.write('PosVel2D', PosVel2D)
+        
+
+    def closeBag(self):
+
+        self.bag.close()
+
+
+def bagToCsv(bag_path, csv_path):
+
+    bag = rosbag.Bag(bag_path, 'r')
+
+    column_names = ['x', 'y']
+    
+
+    data = []
+
+    for topic, msg, t in bag.read_messages(topics = 'PosVel2D'):
+
+        row = {
+            'x': msg.vector.x,
+            'y': msg.vector.y}
+        
+        data.append(row)
+
+    df = pd.DataFrame(data, columns=column_names)
+    print(df.head())
+    df.to_csv(csv_path)
+
+    print('standard deviation:')
+    print(df.std())
+
+    bag.close()
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('ball_position_recorder', anonymous=True, disable_signals=True)
+
+    bag_file_path = 'src/ball_controller/data/position.bag'
+    csv_file_path = 'src/ball_controller/data/position.csv'
+    
+    try:
+        recorder = SkinForceRecorder(bag_file_path, csv_file_path)
+        print('recording...')
+        rospy.spin()
+
+    except KeyboardInterrupt:
+        recorder.closeBag()
+        print('Closed ros bag successfully.')
+
+        bagToCsv(bag_file_path, csv_file_path)
+        print('Converted bag to csv successfully.')
+
+
+
+        rospy.signal_shutdown('manually shutdown ros node.')
